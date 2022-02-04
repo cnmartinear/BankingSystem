6d8e3f2d4622e94a5c2620a29d675cea31c02b4c
Chelsie Martinear <cnmartinear@gmail.com>
2022-02-04 11:26:07 -0600

    Add project files.

---
+++ Bank.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QBankingSystem
+{
+    public class Bank
+    {
+        public string Name;
+        public List<Account> Accounts = new List<Account>();
+
+        //add bank to system
+        public Bank(string name)
+        {
+            Name = name;
+        }
+
+        //retrieve list of bank's accounts
+        public void GetAccounts()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Bank: {0}\n", Name);
+            sb.AppendLine();
+
+            sb.AppendLine("Accounts");
+            sb.AppendLine("--------");
+
+            foreach (Account account in Accounts)
+            {
+                sb.AppendLine("Owner: " + account.Owner);
+                sb.AppendLine("Type: " + account.Type);
+                if (account.Type == AccountType.INVESTMENT) sb.AppendLine("Type:" + account.InvType);
+                sb.AppendLine("Balance: $" + account.Balance);
+                sb.AppendLine();
+            }
+
+            Console.Write(sb.ToString());
+        }
+
+        //create new account for this bank: 3 arguments
+        public Account OpenCheckingAccount(string owner, double balance)
+        {
+            Account account = new Account(owner, balance, AccountType.CHECKING);
+            Accounts.Add(account);
+            return account;
+        }
+
+        //create new account for this bank: 4 arguments
+        public Account OpenInvestmentAccount(string owner, double balance, InvestmentAccountType invType)
+        {
+            Account account = new Account(owner, balance, AccountType.INVESTMENT, invType);
+            Accounts.Add(account);
+            return account;
+        }
+
+        //remove account from this bank
+        public void CloseAccount(Account account)
+        {
+            Accounts.Remove(account);
+        }
+    }
+}
